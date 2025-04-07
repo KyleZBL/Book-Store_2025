94d970be9f7569a7b81b0ef574c08e73824c5faa
Kyle Z <kylezembol@gmail.com>
2025-04-07 05:18:37 -0400

    Added Author ID input. Verified Create and Delete features. Updated H elements

--- Models/book.cs
+++ Models/book.cs
@@ -15,7 +15,8 @@ namespace Book_Store.Models
         public DateTime PublishingDate { get; set; }
 
 
-   
+        public Book() { }
+
         // Constructor with full properties
         public Book(int bookID, string isbn, string title, int authorID, string genre, decimal price, int stock, int pages, DateTime publishingDate)
         {
